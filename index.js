54e11c7efc2b6af4909b8f52099733544b3328c5
afnanhussain2002 <afnanhussain2022@gmail.com>
2023-10-21 04:28:36 +0600

    set user info on mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -11,7 +11,7 @@ app.use(cors())
 
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.ebhxdyy.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -25,7 +25,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const carCollection = client.db('mainDB').collection('cars')
+    const carCollection = client.db('mainDB').collection('cars');
+    const addToCartCollection = client.db('mainDB').collection('cart');
+    const membersCollection = client.db('mainDB').collection('member');
+    app.get('/products',async(req,res)=>{
+      const cursor = carCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+    app.get('/products/:id',async(req,res)=>{
+      const id = req.params.id;
+      const quary = {_id: new ObjectId(id)}
+      const result = await carCollection.findOne(quary)
+      res.send(result)
+    })
+    app.get('/products/:brand',async(req,res)=>{
+      const brand = req.params.brand;
+      const quary = {carBrand: brand}
+      const result = await carCollection.find(quary).toArray();
+      res.send(result)
+    })
     app.post('/products',async(req,res)=>{
       const cars = req.body;
       const result = await carCollection.insertOne(cars)
@@ -34,16 +53,60 @@ async function run() {
 
     })
 
-    app.get('/products',async(req,res)=>{
-      const cursor = carCollection.find()
-      const result = await cursor.toArray()
+    app.post('/cart',async(req,res)=>{
+      const cars = req.body
+      const result = await addToCartCollection.insertOne(cars)
+      console.log(result)
+      res.send(result)
+    })
+  app.get('/cart',async(req,res)=>{
+    const cursor = addToCartCollection.find()
+    const result = await cursor.toArray()
+    res.send(result)
+  })
+    app.put('/products/:id',async(req,res)=>{
+      const id = req.params.id
+      console.log(id);
+      const filter = {_id: new ObjectId(id)}
+      console.log(filter);
+      const options = {upsert: true};
+      const updateCar = req.body;
+      const products = {
+        $set:{
+          carName: updateCar.carName,
+          carPhoto: updateCar.carPhoto,
+          carModel: updateCar.carModel,
+          carBrand: updateCar.carBrand,
+          carPrice: updateCar.carPrice,
+          carDescription: updateCar.carDescription,
+          rating: updateCar.rating
+        }
+        
+      }
+      const result = await carCollection.updateOne(filter, products, options)
+      res.send(result)
+    })
+    app.delete('/cart/:id',async(req,res)=>{
+      const id = req.params.id;
+      const quary = {_id: id}
+      const result = await addToCartCollection.deleteOne(quary)
       res.send(result)
     })
 
-    app.get('/products/:brand',async(req,res)=>{
-      const brand = req.params.brand;
-      const quary = {carBrand: brand}
-      const result = await carCollection.find(quary).toArray();
+    // work with user data
+
+    app.get('/users',async(req,res)=>{
+      const cursor = membersCollection.find()
+      const result = await cursor.toArray()
+      res.send(result);
+      
+    })
+
+    app.post('/users',async(req,res)=>{
+      const user = req.body
+      console.log(user);
+      const result = await membersCollection.insertOne(user)
+      console.log(result)
       res.send(result)
     })
 
